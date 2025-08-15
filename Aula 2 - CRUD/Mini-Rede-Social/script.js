4c5973548861efda745d8cfb80c8c390ce4391e4
tigrelau <lauratigreamaral@gmail.com>
2025-08-14 21:58:20 -0300

    add: new

--- Aula 2 - CRUD/Mini-Rede-Social/script.js
+++ Aula 2 - CRUD/Mini-Rede-Social/script.js
@@ -18,3 +18,62 @@ let posts = [
         date: "12/10/2023 12:00:00"
     }
 ];
+
+window.onload = function(){
+    mostrarPosts();
+    document.querySelector("#postForm").addEventListener("submit", addPost)
+}
+
+//CREATE
+function addPost(infosDoEvento){
+    infosDoEvento.preventDefault();
+
+    const textoPost = document.querySelector("#postText").value;
+    const categoriaPost = document.querySelector("#postCategory").value;
+    const imagemPost = document.querySelector("#postImage").value;
+    const dataPost = new Date().toLocaleString();
+
+    const novoPost = {
+        text: textoPost,
+        category: categoriaPost,
+        image: imagemPost,
+        date: dataPost
+    }
+
+    posts.unshift(novoPost);
+
+    mostrarPosts()
+
+}
+//READ
+function mostrarPosts(){
+    //pega a div que vão aparecer os posts
+    const listaPosts= document.querySelector("#postList");
+    listaPosts.innerHTML = "";
+
+    //passa em cada item o array criando um tweet
+    posts.forEach(pegaItem =>{
+        const cardPost = document.createElement("div")
+        cardPost.classList.add("card")
+
+        cardPost.innerHTML = `
+        <h2>${pegaItem.text}</h2>
+        <p>${pegaItem.category}</p>
+        ${pegaItem.image ? `<img src="${pegaItem.image}">`: ""}
+        <p>${pegaItem.date}</p>
+        
+        `
+    
+
+        //adiciona o tweet ao html
+        listaPosts.append(cardPost)
+
+        //atualiza a página
+    
+    })
+
+}
+//UPDATE
+function editarPost(){}
+//DELETE
+function deletarPost(){}
