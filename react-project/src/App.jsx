cefa73fc0e88e2fa013a630354733c7be3092952
tigrelau <lauratigreamaral@gmail.com>
2025-08-28 20:53:34 -0300

    add

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -2,12 +2,15 @@
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <>
-     
+     <Header/>
+     <Header/>
+     <Header/>
     </>
   )
 }
---
+++ react-project/src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <div>
+      <h1> slk vei </h1>
+      <p></p>
+    </div>
+  );
+}
+export default Header;
