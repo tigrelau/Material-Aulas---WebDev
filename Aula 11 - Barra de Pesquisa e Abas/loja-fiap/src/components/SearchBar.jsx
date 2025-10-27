a73b0d96b7f9fe892b6c4ac8f3ada1d9a16195f7
Caio Oliveira <caio.oliveira@grupoviseu.com.br>
2025-10-26 23:27:52 -0300

    Ajuste

--- Aula 11 - Barra de Pesquisa e Abas/loja-fiap/src/components/SearchBar.jsx
+++ Aula 11 - Barra de Pesquisa e Abas/loja-fiap/src/components/SearchBar.jsx
@@ -9,7 +9,7 @@ const SearchBar = () => {
             className="pl-10 pr-3 py-1 w-64 bg-transparent border-b-2 border-black focus:border-black outline-none transition-colors duration-300"
           />
 
-        <ul className="hiddenabsolute top-full left-0 w-full bg-white border shadow mt-1 z-10 max-h-60 overflow-y-auto">
+        <ul className="hidden absolute top-full left-0 w-full bg-white border shadow mt-1 z-10 max-h-60 overflow-y-auto">
             <li> Itens  </li>
         </ul>
         
